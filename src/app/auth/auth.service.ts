019108357ae1447e98fb3a4a960bb1ec6a1b334f
wiltrahan <twilorip@gmail.com>
2019-01-09 19:16:11 -0500

    add logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,17 +1,23 @@
 import { Subject } from 'rxjs';
 import { AuthData } from './auth-data.model';
 import { User } from './user.model';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
+@Injectable()
 export class AuthService {
+
   authChange = new Subject<boolean>();
   private user: User;
+
+  constructor(private router: Router) {}
 
   registerUser(authData: AuthData) {
     this.user = {
       email: authData.email,
       userId: Math.round(Math.random() * 10000).toString()
     };
-    this.authChange.next(true);
+    this.authSuccessfully();
   }
 
   login(authData: AuthData) {
@@ -19,12 +25,13 @@ export class AuthService {
       email: authData.email,
       userId: Math.round(Math.random() * 10000).toString()
     };
-    this.authChange.next(true);
+    this.authSuccessfully();
   }
 
   logout() {
     this.user = null;
     this.authChange.next(false);
+    this.router.navigate(['/login']);
   }
 
   getUser() {
@@ -33,5 +40,10 @@ export class AuthService {
 
   isAuth() {
     return this.user != null;
+  }
+
+  private authSuccessfully() {
+    this.authChange.next(true);
+    this.router.navigate(['/training']);
   }
 }
